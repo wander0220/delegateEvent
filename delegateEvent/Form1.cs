9ec24406d0dd6c1333ac8a7ccfd53df266d8956c
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-09-29 10:26:34 +0900

    초기 커밋.

---
+++ delegateEvent/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace delegateEvent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Button button = new Button();
+            button.Text = "버튼";
+            button.Click += delegate
+        }
+
+
+    }
+}
